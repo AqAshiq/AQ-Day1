09f4f48710bc68b4f0a8c63fa10191baf674959f
AqAshiq <ashiq.ar7@gmail.com>
2022-12-20 10:34:19 -0800

    my task

---
+++ src/gitaq/Ashiq.java
@@ -0,0 +1,33 @@
+package gitaq;
+
+public class Ashiq {
+	
+	
+	public void m1() {
+
+		System.out.println("Aq");
+	}
+	
+	public void m2() {
+
+		System.out.println("ki");
+	}
+	
+	public void m3() {
+		System.out.println("kkkk");
+	}
+   
+	public static void main(String[] args) {
+	
+		Ashiq g = new Ashiq();
+	
+		g.m1();
+	
+		g.m2();
+	
+		g.m3();
+	
+}
+
+
+}
